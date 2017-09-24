733c8dbb44c4e92a8623691843c745d700f22b06
Michael Stringer <stringbean@users.noreply.github.com>
2017-09-24 18:54:16 +0100

    Tidy command line argument generation

--- src/main/scala/org/scalasbt/findbugs/FindbugsRunner.scala
+++ src/main/scala/org/scalasbt/findbugs/FindbugsRunner.scala
@@ -15,14 +15,16 @@ package org.scalasbt.findbugs
 import java.io.File
 
 import sbt.Keys.{Classpath, TaskStreams}
-import sbt._
+import sbt.{File, _}
 
 import scala.util.control.NonFatal
+import scala.xml.{Node, XML}
 
 object FindbugsRunner {
+  private val FindbugsMainClass = "edu.umd.cs.findbugs.LaunchAppropriateUI"
+
   def runFindBugs(
       findbugsClasspath: Classpath,
-      compileClasspath: Classpath,
       paths: PathSettings,
       filters: FilterSettings,
       misc: MiscSettings,
@@ -30,77 +32,92 @@ object FindbugsRunner {
       streams: TaskStreams
   ): Unit = {
     IO.withTemporaryDirectory { filterPath =>
-      val cmd = commandLine(findbugsClasspath, compileClasspath, paths, filters, filterPath, misc, streams)
+      val cmd = buildCommandLine(findbugsClasspath, paths, filters, misc, filterPath)
+
+      streams.log.debug("Executing FindBugs command line.")
+      streams.log.debug(s"Output file: ${paths.reportPath.toString}")
+      streams.log.debug(s"Analyzed path: ${paths.analyzedPath.toString}")
+
+      paths.reportPath foreach { path =>
+        IO.createDirectory(path.getParentFile)
+      }
+
       FindbugsRunner.executeCommandLine(cmd, javaHome, streams.log)
     }
   }
 
-  // TODO split
-  // scalastyle:off method.length
-  private def commandLine(
+  private[findbugs] def buildCommandLine(
       findbugsClasspath: Classpath,
-      compileClasspath: Classpath,
       paths: PathSettings,
       filters: FilterSettings,
-      filterPath: File,
       misc: MiscSettings,
-      streams: TaskStreams): List[String] = {
+      tmpDir: File): Seq[String] = {
 
-    def findbugsCommandLine = findbugsJavaCall ++ findbugsCallOptions ++ findbugsCallArguments
-
-    def findbugsJavaCall = {
-      val classpath = commandLineClasspath(findbugsClasspath.files)
-      streams.log.debug("FindBugs classpath: %s" format classpath)
-
-      List("-Xmx%dm".format(misc.maxMemory), "-cp", classpath, "edu.umd.cs.findbugs.LaunchAppropriateUI", "-textui")
-    }
-
-    def findbugsCallArguments = paths.analyzedPath map (_.getPath)
-
-    def findbugsCallOptions = {
-      if (paths.reportPath.isDefined && misc.reportType.isEmpty) {
-        sys.error("If a report path is defined, a report type is required!")
-      }
-
-      val auxClasspath = paths.auxPath ++ (findbugsClasspath.files filter (_.getName startsWith "jsr305"))
-
-      addOnlyAnalyzeParameter(
-        addSortByClassParameter(
-          Filters.addFilterFiles(
-            filters,
-            filterPath,
-            misc.reportType.map(t => List(t.arg)).getOrElse(Nil) ++
-              paths.reportPath.map(path => List("-output", path.absolutePath)).getOrElse(Nil) ++ List(
-              "-nested:%b".format(misc.analyzeNestedArchives),
-              "-auxclasspath",
-              commandLineClasspath(auxClasspath),
-              misc.priority.arg,
-              "-effort:%s".format(misc.effort.value)
-            )
-          )))
-    }
-
-    def addOnlyAnalyzeParameter(arguments: List[String]) = misc.onlyAnalyze match {
-      case None => arguments
-      case Some(Nil) => arguments
-      case Some(packagesAndClasses) => arguments ++ List("-onlyAnalyze", packagesAndClasses mkString ",")
-    }
-
-    def addSortByClassParameter(arguments: List[String]) =
-      arguments ++ (if (misc.sortReportByClassNames) "-sortByClass" :: Nil else Nil)
-
-    def commandLineClasspath(classpathFiles: Seq[File]) = PathFinder(classpathFiles).absString
-
-    streams.log.debug("Executing FindBugs command line.")
-    streams.log.debug("Output file: " + paths.reportPath.toString)
-    streams.log.debug("Analyzed path: " + paths.analyzedPath.toString)
-
-    paths.reportPath foreach (path => IO.createDirectory(path.getParentFile))
-
-    findbugsCommandLine
+    baseCommand(findbugsClasspath, paths, misc) ++
+      miscSettingArgs(misc) ++
+      FilterUtils.generateFilterFiles(filters, tmpDir) ++
+      pathSettingArgs(findbugsClasspath, paths) ++
+      analyzedPaths(paths)
   }
 
-  private def executeCommandLine(commandLine: List[String], javaHome: Option[File], log: Logger): Unit = {
+  private def baseCommand(findbugsClasspath: Classpath, paths: PathSettings, misc: MiscSettings): Seq[String] = {
+    Seq(
+      s"-Xmx${misc.maxMemory}m",
+      "-cp",
+      PathFinder(findbugsClasspath.files).absString,
+      FindbugsMainClass,
+      "-textui"
+    ) ++
+      misc.reportType.map(t => List(t.arg)).getOrElse(Nil)
+  }
+
+  private def miscSettingArgs(misc: MiscSettings): Seq[String] = {
+    val onlyArgs = misc.onlyAnalyze match {
+      case None | Some(Nil) => Nil
+      case Some(only) => Seq("-onlyAnalyze", only.mkString(","))
+    }
+
+    Seq(
+      "-effort:%s".format(misc.effort.value),
+      "-nested:%b".format(misc.analyzeNestedArchives),
+      misc.priority.arg
+    ) ++
+      (if (misc.sortReportByClassNames) Seq("-sortByClass") else Nil) ++
+      onlyArgs
+  }
+
+  private def analyzedPaths(paths: PathSettings): Seq[String] = {
+    paths.analyzedPath.map(_.absolutePath)
+  }
+
+  private def pathSettingArgs(findbugsClasspath: Classpath, paths: PathSettings): Seq[String] = {
+    val auxClasspath = findbugsClasspath.files.filter(_.getName startsWith "jsr305") ++ paths.auxPath
+
+    Seq(
+      "-auxclasspath",
+      auxClasspath.map(_.absolutePath).mkString(File.pathSeparator)
+    ) ++
+      paths.reportPath.map(path => Seq("-output", path.absolutePath)).getOrElse(Nil)
+  }
+
+  def addFilterFiles(filters: FilterSettings, filterPath: File, options: List[String]): List[String] = {
+    def addIncludeFilterFile(options: List[String]) = addFilterFile(options, filters.includeFilters, "include")
+    def addExcludeFilterFile(options: List[String]) = addFilterFile(options, filters.excludeFilters, "exclude")
+
+    def addFilterFile(options: List[String], maybeFilters: Option[Node], kindOfFilter: String): List[String] = {
+      options ++ maybeFilters
+        .map({ f =>
+          val filterFile = (filterPath / "%sFilterFile.xml".format(kindOfFilter.capitalize)).getAbsolutePath
+          XML.save(filterFile, f, "UTF-8")
+          List(s"-$kindOfFilter", filterFile)
+        })
+        .getOrElse(Nil)
+    }
+
+    addExcludeFilterFile(addIncludeFilterFile(options))
+  }
+
+  private def executeCommandLine(commandLine: Seq[String], javaHome: Option[File], log: Logger): Unit = {
     try {
       val forkOptions = ForkOptions(
         javaHome,
